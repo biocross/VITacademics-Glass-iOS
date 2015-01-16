0c208caef712635a5f9754a37255b3b5f48e8d96
Siddharth Gupta <sids.1992@gmail.com>
2015-01-16 19:14:46 +0530

    Added Pods & Lines
    Graph
    CocoaPods integrated

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,44 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Mantle
+#define COCOAPODS_POD_AVAILABLE_Mantle
+#define COCOAPODS_VERSION_MAJOR_Mantle 1
+#define COCOAPODS_VERSION_MINOR_Mantle 5
+#define COCOAPODS_VERSION_PATCH_Mantle 3
+
+// Mantle/extobjc
+#define COCOAPODS_POD_AVAILABLE_Mantle_extobjc
+#define COCOAPODS_VERSION_MAJOR_Mantle_extobjc 1
+#define COCOAPODS_VERSION_MINOR_Mantle_extobjc 5
+#define COCOAPODS_VERSION_PATCH_Mantle_extobjc 3
+
+// ReactiveCocoa
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa 4
+
+// ReactiveCocoa/Core
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_Core
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_Core 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_Core 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_Core 4
+
+// ReactiveCocoa/UI
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_UI
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_UI 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_UI 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_UI 4
+
+// ReactiveCocoa/no-arc
+#define COCOAPODS_POD_AVAILABLE_ReactiveCocoa_no_arc
+#define COCOAPODS_VERSION_MAJOR_ReactiveCocoa_no_arc 2
+#define COCOAPODS_VERSION_MINOR_ReactiveCocoa_no_arc 4
+#define COCOAPODS_VERSION_PATCH_ReactiveCocoa_no_arc 4
+
--- VITacademics-Glass/GraphView.m
+++ VITacademics-Glass/GraphView.m
@@ -7,6 +7,8 @@
 //
 
 #import "GraphView.h"
+#import "MultiplePulsingHaloLayer.h"
+#import "PulsingHaloLayer.h"
 
 @implementation GraphView
 
@@ -34,18 +36,45 @@
 
 - (void)drawRect:(CGRect)rect
 {
-    [[UIColor whiteColor] setStroke];
     
     
-    int percentage = self.currentValue * 100;
+    //int percentage = self.currentValue * 100;
     
-    [[UIColor colorWithRed:0.8 green:0.8 blue:1 alpha:1] setStroke];
+    [[UIColor yellowColor] setStroke];
+    
+    
+    
+    
     
     [self.path moveToPoint:CGPointMake(-rect.size.width/2, rect.size.height - self.beforeValue*rect.size.height)];
     [self.path addLineToPoint:CGPointMake(rect.size.width/2, rect.size.height - self.currentValue*rect.size.height)];
     [self.path addLineToPoint:CGPointMake(rect.size.width*3/2, rect.size.height - self.afterValue*rect.size.height)];
-    
+    [self.path setLineWidth:2.5];
     [self.path stroke];
+    
+    
+    /*MultiplePulsingHaloLayer *halo = [[MultiplePulsingHaloLayer alloc] initWithHaloLayerNum:3 andStartInterval:0.7];
+    
+    [halo buildSublayers];
+    
+    PulsingHaloLayer *halo = [PulsingHaloLayer layer];
+    
+    halo.position = CGPointMake(rect.size.width/2, rect.size.height - self.currentValue*rect.size.height);
+    halo.radius = rect.size.width/2;
+    
+    
+    
+    
+    halo.backgroundColor = [UIColor redColor].CGColor;
+    [self.layer addSublayer:halo];
+    
+    if(percentage > 74){
+        halo.radius = 20;
+        halo.backgroundColor = [UIColor colorWithRed:0.2
+                                               green:0.2
+                                                blue:0.9
+                                               alpha:1.0].CGColor;
+    }*/
 }
 
 
